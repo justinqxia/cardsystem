5d0295cf5c83588488c53358c84e6e2a955a5add
Justin Xia <justinqxia@gmail.com>
2022-03-09 17:26:54 -0500

    changed clearing of cards and slots

--- betareader.py
+++ betareader.py
@@ -36,6 +36,8 @@ def readSwipe():
                 fetchdata(row)
                 signout(row)
                 getstatus(row)
+                clearcard(row)
+                updatecard(row)
                 movetoslots(row)
                 checkform(row)
                 if((row[5]=="White")and(len(row[6])!=0)):
@@ -43,10 +45,10 @@ def readSwipe():
                 elif(len(row[6])!=0):
                     print(row[8]+" "+row[1]+" "+row[2]+" signed out")
                 print("")
-            else:
-                signin(row)                
+            else:          
                 clearcard(row)
                 clearslots(row)
+                signin(row)
                 now = datetime.now()
                 returntime = now.strftime("%H:%M:%S")
                 if(row[5]=="White"):
@@ -258,7 +260,7 @@ def shortcut(cardinfo):
         print("List of shortcuts:")
         print("1 - exit program")
         print("2 - list of everyone signed out right now")
-        print("3 - list of everyone signed out before 6:30")
+        print("3 - Card check (Does not return Blue cards)")
         print("4 - list of people signed out with a particular card color")
         print("5 - Edit someone's card")
         print("6 - Clear someone's card")
@@ -272,7 +274,8 @@ def listout(row):
 
 #cardcheck function
 def cardcheck(row):
-    if((row[8]<'18:30:00')and(len(row[8])!=0)):
+    nonblue=["Pink","Yellow","Green"]
+    if((row[8]<'18:30:00')and(len(row[8])!=0)and(row[5] in nonblue)):
         print(row[1]+" "+row[2])
 
 #curfew function
@@ -365,8 +368,8 @@ def movetoslots(row):
 
     mycursor = mydb.cursor()
 
-    sql = "INSERT INTO slots (destination, companion, return1, sp, id, email, firstname, lastname, floor, signout,status) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s)"
-    val = (row[6],row[11],row[9],row[12],row[0],row[3],row[1],row[2],row[4],row[8],row[13])
+    sql = "INSERT INTO slots (destination, companion, return1, sp, id, email, firstname, lastname, floor, signout,status,color) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s,%s)"
+    val = (row[6],row[11],row[9],row[12],row[0],row[3],row[1],row[2],row[4],row[8],row[13],row[5])
     mycursor.execute(sql, val)
 
     mydb.commit()
@@ -391,10 +394,31 @@ def getstatus(row):
     for block in forms:
         for x in block:
             line.append(x)
+        if(line[1]=="Other"):
+            line.pop(1)
+        else:
+            line.pop(2)
         if(row[3] == line[0]):
             row[13]=line[1]
             return row
         line=[]
+
+#updates cards database
+def updatecard(row):
+    mydb = mysql.connector.connect(
+    host="localhost", 
+    user="root",
+    password="", 
+    database="phplogin"
+    )
+
+    mycursor = mydb.cursor()
+
+    sql = "INSERT INTO cards (destination, companion, return1, sp, id, email, firstname, lastname, floor, signout,color) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s)"
+    val = (row[6],row[11],row[9],row[12],row[0],row[3],row[1],row[2],row[4],row[8],row[5])
+    mycursor.execute(sql, val)
+
+    mydb.commit()
 
 #Changes value on someon'es card
 def changecard(row):
@@ -408,22 +432,7 @@ def changecard(row):
     clearslots(row)
     clearcard(row)
     movetoslots(row)
-
-    #writes change to cards database
-    mydb = mysql.connector.connect(
-    host="localhost", 
-    user="root",
-    password="", 
-    database="phplogin"
-    )
-
-    mycursor = mydb.cursor()
-
-    sql = "INSERT INTO cards (destination, companion, return1, sp, id, email, firstname, lastname, floor, signout) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    val = (row[6],row[11],row[9],row[12],row[0],row[3],row[1],row[2],row[4],row[8])
-    mycursor.execute(sql, val)
-
-    mydb.commit()
+    updatecard(row)
 
     return row
 
