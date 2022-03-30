2bca4227b06ed0727e725d467dcf53dab9c495df
henryo2005 <97057523+henryo2005@users.noreply.github.com>
2022-03-30 14:45:34 -0400

    Update lcdLedIndicator.ino

--- lcdLedIndicator.ino
+++ lcdLedIndicator.ino
@@ -3,14 +3,16 @@
 //Initialise the LCD with the arduino. LiquidCrystal(rs, enable, d4, d5, d6, d7)
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 int incomingByte;
-const int ledSuccess = 6;
+const int ledIn = 8;
 const int ledFailure = 7;
+const int ledOut = 6;
 
 void setup() {
   // Switch on the LCD screen
   Serial.begin(9600);
   lcd.begin(16, 2);
-  pinMode(ledSuccess,OUTPUT);
+  pinMode(ledIn,OUTPUT);
+  pinMode(ledOut,OUTPUT);
   pinMode(ledFailure,OUTPUT);
 }
 
@@ -18,19 +20,23 @@ void loop() {
   if (Serial.available() > 0) {
     incomingByte = Serial.read();
     if (incomingByte == 'A') {
-      digitalWrite(ledSuccess,HIGH);
+      digitalWrite(ledOut,HIGH);
     }
     if (incomingByte == 'B') {
-      digitalWrite(ledFailure,HIGH);
-      lcd.print("Swipe Error");
+      digitalWrite(ledIn,HIGH);
     }
     if (incomingByte == 'C') {
       digitalWrite(ledFailure,HIGH);
-      lcd.print("Form Empty");
+      lcd.print("Swipe Error");
     }
     if (incomingByte == 'D') {
+      digitalWrite(ledFailure,HIGH);
+      lcd.print("Form Empty");
+    }
+    if (incomingByte == 'E') {
       digitalWrite(ledFailure,LOW);
-      digitalWrite(ledSuccess,LOW);
+      digitalWrite(ledOut,LOW);
+      digitalWrite(ledIn,LOW);
       lcd.clear();
     }
   }
